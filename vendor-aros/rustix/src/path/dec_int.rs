c8a6030be36d0c5979a3b448901fc5dfe27ad709
John KNIPPER <code@jkn.me>
2026-07-23 11:45:06 +0200

    rustix/aros: use std::os::aros::ffi for OsStrExt in path handling

--- vendor-aros/rustix/src/path/dec_int.rs
+++ vendor-aros/rustix/src/path/dec_int.rs
@@ -14,6 +14,8 @@ use core::mem::{self, MaybeUninit};
 use core::num::{NonZeroU8, NonZeroUsize};
 #[cfg(all(feature = "std", unix))]
 use std::os::unix::ffi::OsStrExt;
+#[cfg(all(feature = "std", target_os = "aros"))]
+use std::os::aros::ffi::OsStrExt;
 #[cfg(all(
     feature = "std",
     target_os = "wasi",
